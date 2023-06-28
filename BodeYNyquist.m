7a1ce5f6885282fc0aa44a239294ba9d584e918a
nahu_ <nahu_@DESKTOP-HE0I1DC.fibertel.com.ar>
2023-06-28 16:45:18 -0300

    bode y nyquist

--- BodeYNyquist.m
+++ BodeYNyquist.m
@@ -1,19 +1,38 @@
 %% Diagramas de Nyquist
-%k=3.9375
-k=3.9375
+%% k=1
+k=1
 G1=tf([1],[1 1]);
 G2=tf([k],[1 1 1]);
 H=tf([1],[1 2]);
 FTLA=G1*G2*H;
-FTLC=G1*G2/(1+G1*G2*H); %O FTLC=feedblack(G1*G2,H)
+FTLC=G1*G2/(1+G1*G2*H); % O FTLC=feedblack(G1*G2,H)
 figure(1);
 rlocus(FTLA);
 figure(2);
 pzmap(FTLA);
 figure(3);
 bode(FTLA);
+legend('k=1');
 figure(4);
 nyquist(FTLA);
+legend('k=1');
+%% k=3.9375
+k=3.9375
+G1=tf([1],[1 1]);
+G2=tf([k],[1 1 1]);
+H=tf([1],[1 2]);
+FTLA=G1*G2*H;
+FTLC=G1*G2/(1+G1*G2*H); % O FTLC=feedblack(G1*G2,H)
+figure(1);
+rlocus(FTLA);
+figure(2);
+pzmap(FTLA);
+figure(3);
+bode(FTLA);
+legend('k=3.9375');
+figure(4);
+nyquist(FTLA);
+legend('k=3.9375');
 
 %% k=10
 
@@ -29,5 +48,29 @@ figure(2);
 pzmap(FTLA);
 figure(3);
 bode(FTLA);
+legend('k=10');
 figure(4);
 nyquist(FTLA);
+legend('k=10');
+%% los 3
+k=1
+k2=3.9375
+k3=10
+G1=tf([1],[1 1]);
+G2=tf([k],[1 1 1]);
+G3=tf([k2],[1 1 1]);
+G4=tf([k3],[1 1 1]);
+H=tf([1],[1 2]);
+FTLA1=G1*G2*H;
+FTLC1=G1*G2/(1+G1*G2*H); % O FTLC=feedblack(G1*G2,H)
+FTLA2=G1*G3*H;
+FTLC2=G1*G3/(1+G1*G3*H);
+FTLA3=G1*G4*H;
+FTLC3=G1*G4/(1+G1*G4*H);
+
+figure(3);
+bode(FTLA1,FTLA2,FTLA3);
+legend('k=1','k=3.9375','k=10');
+figure(4);
+nyquist(FTLA1,FTLA2,FTLA3);
+legend('k=1','k=3.9375','k=10');
--- sist1erOrden.m
+++ sist1erOrden.m
@@ -15,3 +15,22 @@ title("Respuestas al escalon en sistemas de 1er orden")
 ylabel("Amplitud")
 xlabel("Tiempo")
 legend('G1(t), Tau=0.1','G2(t), Tau=0.2')
+%% 
+% Parámetros del sistema
+
+z='' %Sin ceros
+
+% Sistema de segundo orden1
+p=-1
+k=-p;
+[num,dem]=zp2tf(z,p,k) %Pasa de ceros y polos a funcion de transferencia(num y denominador)
+G=tf(num,dem)
+% Respuesta al escalón
+figure;
+step(G);
+title('Respuesta al escalón');
+
+% Respuesta al impulso
+figure;
+impulse(G);
+title('Respuesta al impulso');
